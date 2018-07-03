48be3639bf30e3256940cb6e7de1d2a157666608
danyhp <dany.panggabean@gmail.com>
2018-07-03 12:19:08 +0700

    #1

---
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/MerchantLoginActivity.java
@@ -0,0 +1,13 @@
+package com.mmithb.danyhp.pohonkoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MerchantLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_login);
+    }
+}
---
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/Model/User.java
@@ -0,0 +1,17 @@
+package com.mmithb.danyhp.pohonkoin.Model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    public String name;
+    public String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
