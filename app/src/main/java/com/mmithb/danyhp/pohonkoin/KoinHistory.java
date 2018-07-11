fd4c1c7f56aef838a12493a291b1faa4a24b8925
calvinbudianto <calvinbudianto16@yahoo.com>
2018-07-11 14:42:01 +0700

    #5

---
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/KoinHistory.java
@@ -0,0 +1,13 @@
+package com.mmithb.danyhp.pohonkoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KoinHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koin_history);
+    }
+}
