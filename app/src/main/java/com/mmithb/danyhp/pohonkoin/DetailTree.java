19cd25fddf048b0309e56ce0f4fb24d6f6468e72
calvinbudianto <calvinbudianto16@yahoo.com>
2018-07-18 09:55:41 +0700

    #5

---
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/DetailTree.java
@@ -0,0 +1,13 @@
+package com.mmithb.danyhp.pohonkoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailTree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tree);
+    }
+}
