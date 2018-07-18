fc2098639475bba859e6b60d2a48a49c5a4c93ef
danyhp <dany.panggabean@gmail.com>
2018-07-18 14:35:24 +0700

    #15

--- app/src/main/java/com/mmithb/danyhp/pohonkoin/MerchantLoginActivity.java
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/MerchantLoginActivity.java
@@ -1,13 +1,27 @@
 package com.mmithb.danyhp.pohonkoin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MerchantLoginActivity extends AppCompatActivity {
+
+    private Button signUpMerchantBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merchant_login);
+
+        signUpMerchantBtn = findViewById(R.id.btn_signup_merchant);
+        signUpMerchantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MerchantLoginActivity.this, SignupMerchantActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/mmithb/danyhp/pohonkoin/SignupMerchantActivity.java
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/SignupMerchantActivity.java
@@ -1,13 +1,24 @@
 package com.mmithb.danyhp.pohonkoin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupMerchantActivity extends AppCompatActivity {
+
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_merchant);
+
+        signUpBtn = findViewById(R.id.btn_sign_up);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
