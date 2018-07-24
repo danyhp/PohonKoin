6c99f82a2a81aaf661143f4ac17b46eaaed3d064
calvinbudianto <calvinbudianto16@yahoo.com>
2018-07-24 15:37:04 +0700

    #5

---
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/ImageSliderAdapter.java
@@ -0,0 +1,46 @@
+package com.mmithb.danyhp.pohonkoin;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageSliderAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+    private Integer [] images = {R.drawable.cointree1, R.drawable.cointree2, R.drawable.cointree3};
+
+    public ImageSliderAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public Object instantiateItem(ViewGroup container, int position){
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.sliding, null);
+        ImageView iv = (ImageView)view.findViewById(R.id.gambar_penginapan);
+        iv.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object){
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}
