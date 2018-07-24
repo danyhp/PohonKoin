63548f28d5e09fe5bd73e54db049c2030cb39052
calvinbudianto <calvinbudianto16@yahoo.com>
2018-07-24 16:12:37 +0700

    #5

--- app/src/main/java/com/mmithb/danyhp/pohonkoin/ImageSliderAdapter.java
+++ app/src/main/java/com/mmithb/danyhp/pohonkoin/ImageSliderAdapter.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-
 public class ImageSliderAdapter extends PagerAdapter {
     private Context context;
     private LayoutInflater inflater;
